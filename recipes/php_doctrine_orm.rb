b22719547d27621c1848b6cefd372a36794034dc
Ryan Muller <ryan@muller.io>
2014-04-19 16:08:32 -0400

    Improved

--- recipes/php_doctrine_orm.rb
+++ recipes/php_doctrine_orm.rb
@@ -1,3 +1,12 @@
-package "php-doctrine-orm" do
-  action :upgrade
+php_pear_channel 'pear.symfony.com' do
+  action :discover
+end
+
+dp = php_pear_channel 'pear.doctrine-project.org' do
+  action :discover
+end
+
+php_pear "DoctrineORM" do
+  channel dp.channel_name
+  action :install
 end
--- recipes/php_seclib.rb
+++ recipes/php_seclib.rb
@@ -1,3 +1,93 @@
-package "php-seclib" do
-  action :upgrade
+sl = php_pear_channel 'phpseclib.sourceforge.net' do
+  action :discover
+end
+
+php_pear 'Crypt_AES' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_Base' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_Blowfish' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_DES' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_Hash' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_RC4' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_RSA' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_Rijndael' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_TripleDES' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Crypt_Twofish' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'File_ANSI' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'File_ASN1' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'File_X509' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Math_BigInteger' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Net_SFTP' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Net_SSH1' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'Net_SSH2' do
+  channel sl.channel_name
+  action :install
+end
+
+php_pear 'System_SSH_Agent' do
+  channel sl.channel_name
+  action :install
 end
